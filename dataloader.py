9680f9b2819722a7a49be4925df6db36f7fd225a
Maxim <maxim.kuklin@xperience.ai>
2020-05-04 21:47:20 +0300

    update dataloader and split script

--- dataloader.py
+++ dataloader.py
@@ -1,8 +1,48 @@
+import os
 from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
-import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 class CellDataset(Dataset):
     def __init__(self, root, mode, transforms=None):
+
+        path = os.path.join(root, 'annotations', mode+'.csv')
+        self.mode = mode
+        if self.mode == 'test':
+            self.images_path = os.path.join(root, self.mode)
+        else:
+            self.images_path = os.path.join(root, 'train')
+        self.data = pd.read_csv(path).values
+        self.classes = {cls[-1]: int(cls[-1][6:]) for cls in self.data}
+
+    def __len__(self):
+        return len(self.data)
+
+    def get_image(self, row):
+        path = os.path.join(row[1], 'Plate'+str(row[2]))
+        full_image = []
+        for level in range(1, 7):
+            image_name = '_'.join([row[3], 's'+str(row[4]), 'w'+str(level)])+'.png'
+            image_name = os.path.join(self.images_path, path, image_name)
+            image = np.asarray(Image.open(image_name))
+            full_image.append(image)
+        full_image = np.stack(full_image)
+        return full_image
+
+    def __getitem__(self, idx):
+        line = self.data[idx]
+        image = self.get_image(line)
+
+        plt.imshow(image[3:].transpose(1,2,0))
+        plt.show()
+
+        cls = int(line[-1][6:])
+        return image, cls
+
+dataset = CellDataset('dataset', mode='val')
+
+dataset[0]
 
--- utils/split_dataset.py
+++ utils/split_dataset.py
@@ -1,20 +1,45 @@
 import os
+import copy
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
 def split_train(root):
-    path = os.path.join('..', root, 'train.csv')
+    path = os.path.join(root, 'train.csv')
     file = pd.read_csv(path)
-    columns = file.columns
+    columns = list(file.columns.values)
     lines = file.values
     train, val = train_test_split(lines, train_size=0.75, random_state=42)
-    train = pd.DataFrame(train, columns=columns)
-    train.to_csv('../train.csv', index=False)
-    val = pd.DataFrame(val, columns=columns)
-    val.to_csv('../val.csv', index=False)
+    # train = pd.DataFrame(train, columns=columns)
+    train_with_parts = []
+    for element in train:
+        for i in range(1,3):
+            tmp = copy.deepcopy(element)
+            element = np.append(element, i)
+            element[-1], element[-2] = element[-2], element[-1]
+            train_with_parts.append(element)
+            element = tmp
+
+    val_with_parts = []
+    for element in val:
+        for i in range(1,3):
+            tmp = copy.deepcopy(element)
+            element = np.append(element, i)
+            element[-1], element[-2] = element[-2], element[-1]
+            val_with_parts.append(element)
+            element = tmp
+
+    columns.append('part')
+    columns[-1], columns[-2] = columns[-2], columns[-1]
+
+    train = pd.DataFrame(train_with_parts, columns=columns).sort_values(['id_code', 'part'])
+    val = pd.DataFrame(val_with_parts, columns=columns).sort_values(['id_code', 'part'])
+
+    train.to_csv('../train.csv', index=False, columns=columns)
+    val.to_csv('../val.csv', index=False, columns=columns)
     return True
 
 
 if __name__ == '__main__':
-    split_train('dataset')
+    split_train('/media/data/Projects/course_paper_2020/dataset')
